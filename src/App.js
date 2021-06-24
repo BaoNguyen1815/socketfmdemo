e5556da0a7c8229fd339d1fb727e62da228cf73e
BaoNguyen1815 <baonguyendo1810@gmail.com>
2021-06-24 20:00:17 +0700

    fc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { io } from "socket.io-client";
 
 function App() {
+  const Socket = io("http://150.95.113.87:3002");
+
+  const [listMessages, setListMessages] = useState();
+  const [text, setText] = useState('')
+  useEffect(()=>{
+    fetch('(http://150.95.113.87.3002/api/v1/players/getMessagesByChannel',{
+      method: 'DIEN VAO',
+      body: {
+        data: 'DIEN VAO',
+      }
+    })
+    //DIEN VAO Chỉ lấy danh sách messages, giả sử results trả về là 1 mảng message thì sẽ thế này:
+      .then(results =>setListMessages(results))
+      .catch(e => console.log(e) )
+  },[])
+
+  const sendMessage = ()=>{
+    //DIEN VAO 2 biến kia, đây là hàm gọi mỗi lần mình gửi tin nhắn
+    Socket.emit('insertMessage', ({PlayerMatchPosition._id, ChatChannel._id, message}));
+  }
+
+
+Socket.on('messageFromServer', ({message}) => {
+	const { player_match_position_id ,chat_channel_id, message } = message
+	const newMessages = messagesHistory.concat(message);
+  //DIEN VAO Chỉ lấy danh sách messages, giả sử results trả về là 1 mảng message thì sẽ thế này:
+	setListMessages(newMessages)
+})
+
+  const RenderListMessages = ()=>{
+    if(listMessages){
+      return listMessages.map(message => <div>{message}</div>)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {RenderListMessages()}
+      <input value={text} onChange={(e)=>setText(e.target.value)}></input>
+      <button onClick={()=>sendMessage()}>Send</button>
     </div>
   );
 }
